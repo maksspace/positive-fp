56ecfbc689d05ef8e919ee179523178d340db94a
Maks Smagin <maksspaceworld@yandex.ru>
2017-08-23 18:28:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,32 +5,37 @@ const makeObjectFieldMapper = f => item => f(item);
 const sum = values => values.reduce((total, v) => v + total, 0);
 
 const mapCartPrices = makeObjectFieldMapper(cart => cart.map(item => item.price));
-const makeCurrencyConverter = name => converter => ({
-	name,
-    convert: v => converter(v)
+const makeCurrencyConverter = currency => k => ({
+	currency,
+    convert: v => v * k
 });
 
-// можно с оптимизировать создав функцию создающую этот мссив из настроек
-const currencyConvertes = [
-  makeCurrencyConverter('rubles')(v => v * 0.17),
-  makeCurrencyConverter('euros')(v => v * 0.85),
-  makeCurrencyConverter('dollars')(v => v * 1),
-  makeCurrencyConverter('pounds')(v => v * 0.78),
-  makeCurrencyConverter('yens')(v => v * 1.09),
+const currenciesRates = [
+	{ name: 'rubles',  rate: 59.06  },
+ 	{ name: 'euros',   rate: 0.85   },
+ 	{ name: 'dollars', rate: 1      },
+ 	{ name: 'pounds',  rate: 0.78   },
+ 	{ name: 'yens',    rate: 109.07 }
 ];
 
 const selectedCart = [
-  { price: 20 },
-  { price: 45 },
-  { price: 67 },
-  { price: 1305 }
+	{ price: 20 },
+	{ price: 45 },
+	{ price: 67 },
+	{ price: 1305 }
 ];
 
+const currencyConvertes = currenciesRates.map(
+	cr => makeCurrencyConverter(cr.name)(cr.rate)
+);
+
 const totalCartBuilder = converters => v => 
-	 converters.reduce(total, cv => {...total, [cv.currency]: cv.convert(v)}, {});
-
-const getAbstractTotal = compose(totalCartBuilder, sum, mapCartPrices);
-
-console.log('total euro:', getAbstractTotal(selectedCart)(currencyConvertes))
-
+	converters.reduce((total, cv) => ({
+		...total,
+        [cv.currency]: cv.convert(v)	
+	}), 
+{});
+	 
+const getAbstractTotal = compose(totalCartBuilder(currencyConvertes), sum, mapCartPrices);
+console.log('total:', getAbstractTotal(selectedCart))
 
